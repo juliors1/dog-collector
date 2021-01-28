13a507418daccc0a669fd5d45cd6397226644209
Juliors1 <juliorojassolis@gmail.com>
2021-01-28 17:32:23 -0500

    Add home page

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from .models import Dog
 # Create your views here
 
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html")
 
 
 def about(request):
