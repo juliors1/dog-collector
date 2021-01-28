e4854a56527d792c4f953c634cb2a3d4d8b65d39
Juliors1 <juliorojassolis@gmail.com>
2021-01-28 16:48:57 -0500

    respond to the client's HTTP Request

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+class Dog:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Lolo', 'tabby', 'foul little demon', 3),
+  Dog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Dog('Raven', 'black tripod', '3 legged dog', 4)
+]
 
 # Define the home view
 def home(request):
