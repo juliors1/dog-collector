972a3deb33100a70172b5db909fb2790941e2e42
Juliors1 <juliorojassolis@gmail.com>
2021-01-28 17:21:36 -0500

    create dog in python shell

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
+from .models import Dog
 
-class Dog:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# Create your views here
 
-dogs = [
-  Dog('Lolo', 'tabby', 'foul little demon', 3),
-  Dog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Dog('Raven', 'black tripod', '3 legged dog', 4)
-]
-
-# Define the home view
 def home(request):
     return HttpResponse("<h1>Hello World</h1>")
 
@@ -23,4 +12,5 @@ def about(request):
 
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, "dogs/index.html", {"dogs": dogs})
