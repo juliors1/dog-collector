ab96b1fe6181fdb380b6239af9f79c3b48216457
Juliors1 <juliorojassolis@gmail.com>
2021-01-29 16:05:03 -0500

    Add new URL pattern to show form and create cat.

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("dogs/", views.dogs_index, name="index"),
+    # new route used to show a form and create a dog
     path("dogs/<int:dog_id>/", views.dogs_detail, name="detail"),
-
+    path("dogs/create/", views.DogCreate.as_view(), name="dogs_create"),
 ]
