4654ca8aeabf4b58346ab82438708ba4f957b490
Juliors1 <juliorojassolis@gmail.com>
2021-01-30 12:12:58 -0500

    add 'Feeding Date' label for the dateField

--- main_app/models.py
+++ main_app/models.py
@@ -23,7 +23,7 @@ class Dog(models.Model):
         return reverse("detail", kwargs={"dog_id": self.id})
 
 class Feeding(models.Model):
-    date = models.DateField()
+    date = models.DateField('Feeding Date')
 
     meal = models.CharField(
         max_length=1,
