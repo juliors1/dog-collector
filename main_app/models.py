426a33a07e3036e352405642f60c7d8c93e01f9b
Juliors1 <juliorojassolis@gmail.com>
2021-02-01 18:51:42 -0500

    Creaste Toy Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,10 @@
+from typing import DefaultDict
 from django.db import models
 from django.urls import reverse
 from datetime import date
 
 
-
-MEALS = (
-    ('B', 'Breakfast'),
-    ('L', 'Lunch'),
-    ('D', 'Dinner')
-)
+MEALS = (("B", "Breakfast"), ("L", "Lunch"), ("D", "Dinner"))
 
 
 # Create your models here.
@@ -26,31 +22,38 @@ class Dog(models.Model):
 
     def fed_for_today(self):
         return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
-    class Meta:
-        ordering = ['id']
 
+    class Meta:
+        ordering = ["id"]
 
 
 class Feeding(models.Model):
-    date = models.DateField('Feeding Date')
+    date = models.DateField("Feeding Date")
 
     meal = models.CharField(
         max_length=1,
-        choices = MEALS,
-        default= MEALS[0][0],
-
+        choices=MEALS,
+        default=MEALS[0][0],
     )
 
     # Create a dog_id FK
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
 
-
     # Nice method for obtaining the friendly value of a Field.choice
 
     def __str__(self):
-         return f"{self.get_meal_display()} on {self.date}"
+        return f"{self.get_meal_display()} on {self.date}"
 
     class Meta:
-        ordering = ['-date']
+        ordering = ["-date"]
 
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("toys_detail", kwargs={"pk": self.id})
