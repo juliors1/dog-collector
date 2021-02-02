11f4b2b15f3e08804fefc8cc68dccd30adaf8c11
Juliors1 <juliorojassolis@gmail.com>
2021-02-02 16:17:39 -0500

    import login and UserCreationForm

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Dog, Toy
 from .forms import FeedingForm
 
