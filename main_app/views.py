8a46838ad2673966b66d52649107f33d98b55338
Juliors1 <juliorojassolis@gmail.com>
2021-02-02 16:01:33 -0500

    assign the logged in user to dog

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,13 @@ from .forms import FeedingForm
 class DogCreate(CreateView):
     model = Dog
     fields =  ['name', 'breed', 'description', 'age']
-
+    # This inherited method is called when a
+    # valid cat form is being submitted
+    def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
 class DogUpdate(UpdateView):
     model = Dog
